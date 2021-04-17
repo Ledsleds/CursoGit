d06008c7a7e458692a60da9c76779865ef4a4272
Luis Eduardo <luiseduardoduarte@yahoo.com.br>
2021-04-17 09:50:27 -0300

    Projeteo iniciado:  Exibe Ola Mundo!

---
+++ Teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo1!");
+            Console.ReadLine();
+        }
+    }
+}
